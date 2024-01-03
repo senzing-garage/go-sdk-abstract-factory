84ebb70bbc5717fedb2e4d3e37c4e6e87ecc0194
docktermj <michael@dockter.com>
2024-01-03 10:59:46 -0500

    #133 Migrate repository

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 	"time"
 
 	"github.com/senzing/g2-sdk-go/g2api"
-	"github.com/senzing/go-common/g2engineconfigurationjson"
-	"github.com/senzing/go-common/truthset"
-	"github.com/senzing/go-logging/logging"
-	"github.com/senzing/go-observing/observer"
+	"github.com/senzing-garage/go-common/g2engineconfigurationjson"
+	"github.com/senzing-garage/go-common/truthset"
+	"github.com/senzing-garage/go-logging/logging"
+	"github.com/senzing-garage/go-observing/observer"
 	"github.com/senzing/go-sdk-abstract-factory/factory"
 )
 
