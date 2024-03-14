592013f4cd18dcadf3dab789745ef69e97ab0647
docktermj <michael@dockter.com>
2024-03-14 11:23:04 -0400

    #150 Update to latest pre-V4

--- makefiles/linux.mk
+++ makefiles/linux.mk
@@ -15,7 +15,7 @@ build-osarch-specific: linux/amd64
 
 .PHONY: clean-osarch-specific
 clean-osarch-specific:
-	@docker rm --force senzing-tools-serve-grpc || true
+	@docker rm --force senzing-serve-grpc || true
 	@rm -rf $(TARGET_DIRECTORY) || true
 	@rm -f $(GOPATH)/bin/$(PROGRAM_NAME) || true
 	@rm -rf /tmp/sqlite || true
